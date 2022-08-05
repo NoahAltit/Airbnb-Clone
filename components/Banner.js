dc663bcabcc28bf1813c337ee34a2f0e40e18b3e
Noah Altit <naltit461@gmail.com>
2022-08-05 17:02:03 -0400

    Updated Home Page

--- components/Banner.js
+++ components/Banner.js
@@ -1,19 +1,20 @@
 import Image from "next/image";
+import banner from "../assets/banner.png";
 
 const Banner = () => {
   return (
     <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
-      <Image
-        src="https://links.papareact.com/0fm"
-        layout="fill"
-        objectFit="cover"
-      />
+      <Image src={banner} layout="fill" objectFit="cover" />
 
       <div className="absolute top-1/2 w-full text-center">
-        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
-        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
-          I'm flexible
-        </button>
+        <p className="text-white font-bold lg:text-3xl md:text-2xl sm:text-sm">
+          Book a trip. Host travellers. All on Airbnb.
+        </p>
+
+        <div className="space-x-10 my-5">
+          <button className="homeBtn">Explore</button>
+          <button className="homeBtn">Host</button>
+        </div>
       </div>
     </div>
   );
--- components/Header.js
+++ components/Header.js
@@ -76,12 +76,17 @@ const Header = ({ placeholder }) => {
 
       {/* Right */}
       <div className="flex space-x-4 items-center justify-end text-gray-500">
-        <p className="hidden md:inline cursor-pointer">Become a host</p>
-        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <p className="hidden md:inline py-2 px-3 cursor-pointer hover:headerRight">
+          Become a Host
+        </p>
 
-        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-          <MenuIcon className="h-6 cursor-pointer" />
-          <UserCircleIcon className="h-6 cursor-pointer" />
+        <div className="cursor-pointer hover:headerRight p-2">
+          <GlobeAltIcon className="h-6" />
+        </div>
+
+        <div className="flex cursor-pointer items-center space-x-2 border-2 p-2 rounded-full hover:sideMenu transition duration-200 ease-out">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
         </div>
       </div>
 
